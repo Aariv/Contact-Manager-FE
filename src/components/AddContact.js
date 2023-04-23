af6f5ef2d9717bd8269a849d116be9f635116c68
Aariv <arivbits@gmail.com>
2023-04-23 17:39:31 +0530

    React JS components, Render a List, Use props to pass data

---
+++ src/components/AddContact.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const AddContact = () =>  {
+    return (
+        <div className='ui main'>
+            <h2>Add Contact</h2>
+            <form className='ui form'>
+                <div className='field'>
+                    <label>Name</label>
+                    <input type='text' placeholder='Name'></input>
+                    
+                </div>
+                <div className='field'>
+                    <label>Email</label>
+                    <input type='email' placeholder='Email'></input>
+                    
+                </div>
+
+                <button className='ui button blue'>Add</button>
+            </form>
+        </div>
+    );
+}
+
+export default AddContact;
